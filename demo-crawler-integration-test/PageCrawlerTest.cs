ef872d528fc402f61fff9529dbfc49c8425860bf
gitName <a@b.c>
2020-09-23 18:14:31 +0200

    Added integration test project with some basic tests

--- demo-crawler-integration-test/PageCrawlerTest.cs
+++ demo-crawler-integration-test/PageCrawlerTest.cs
@@ -1,10 +1,62 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using demo_crawler_api;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+using demo_crawler_api.Models;
+using System.Text;
+
 namespace demo_crawler_integration_test
 {
     public class PageCrawlerTest
     {
+        private readonly TestServer _server;
+
+        private readonly HttpClient _client;
+
         public PageCrawlerTest()
         {
+            _server = new TestServer(new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseStartup<Startup>());
+
+            _client = _server.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("https://www.google.com")]
+        [InlineData("https://www.svt.se")]
+        [InlineData("https://www.twitter.com")]
+        public async Task GetScreenshotForValidUrl(string url)
+        {
+            // Act
+            var requestUrl = $"/api/page-crawler?url={url}";
+            var response = await _client.GetAsync(requestUrl);
+            var content = await response.Content.ReadAsStringAsync();
+            var pageResultDto = JsonConvert.DeserializeObject<CrawlerPageResultDto>(content);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotNull(pageResultDto?.FullPageScreenshot);
+        }
+
+        [Theory]
+        [InlineData("https://www.sdkjhaslkjdh.asdjaksjh")]
+        [InlineData("https://www.qweqwe.qweqwe")]
+        public async Task GetErrorMessageForNonExistingSites(string url)
+        {
+            // Act
+            var requestUrl = $"/api/page-crawler?url={url}";
+            var response = await _client.GetAsync(requestUrl);
+            var content = await response.Content.ReadAsStringAsync();
+            
+            // Assert
+            Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+            Assert.True(string.Equals("ruh roh, something bad happened", content, StringComparison.OrdinalIgnoreCase));           
         }
     }
 }
--- demo-crawler-integration-test/StatusAPITest.cs
+++ demo-crawler-integration-test/StatusAPITest.cs
@@ -1,10 +1,64 @@
-﻿using System;
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using demo_crawler_api;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+using demo_crawler_api.Models;
+using System;
+
 namespace demo_crawler_integration_test
 {
     public class StatusAPITest
     {
+        private readonly TestServer _server;
+
+        private readonly HttpClient _client;
+
         public StatusAPITest()
         {
+            _server = new TestServer(new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseStartup<Startup>());
+
+            _client = _server.CreateClient();
         }
+
+        [Fact]
+        public async Task GetCrawlerStatus()
+        {
+            // Act
+            var response = await _client.GetAsync("/api/status");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetCrawlerStatusUpAndRunning()
+        {
+            // Act
+            var response = await _client.GetAsync("/api/status");
+            var content = await response.Content.ReadAsStringAsync();
+            var statusDto = JsonConvert.DeserializeObject<CrawlerStatusDto>(content);
+
+            // Assert
+            Assert.NotNull(statusDto);
+            Assert.True(string.Equals("up and running", statusDto.Status, StringComparison.OrdinalIgnoreCase));
+        }
+
+        [Fact]
+        public async Task GetResourceNotFound()
+        {
+            // Act
+            var response = await _client.GetAsync("/api/nonexistentresource");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+
     }
 }
